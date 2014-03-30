a279c605f6194678f6aabfb1b01812da650e082c
Parlin Yu <bupt.aswmtjdsj@gmail.com>
2014-03-30 18:38:31 -0400

    quick template header

---
+++ head.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <map>
+#include <set>
+#include <bitset>
+#include <queue>
+#include <deque>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <complex>
+using namespace std;
+
+#define len(x) (x).size()
+#define VI vector<int> 
+
+#define PB push_back
+#define MP make_pair
+
+typedef long long int64;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+
+const int inf = ~0u >> 1;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+
+int sgn(double x) {
+	return fabs(x) < eps ? 0:x < -eps?-1:1;
+}
+
+int main() {
+}

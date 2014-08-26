946cd620120fcb95437fe33c05090db6f11c6b07
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2014-08-26 16:16:59 -0400

    got sth wrong

--- cf/461/a.cpp
+++ cf/461/a.cpp
@@ -1,31 +1,25 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+
+const int maxn = 3 * 100000;
+long long a[maxn];
+
 int main() {
 	int n;
-	char m[105][105];
-	cin >> n;
+	scanf(" %d", &n);
+	long long sum = 0;
 	for(int i = 0; i < n; i++) {
-		cin >> m[i];
+		cin >> a[i];
+		sum += a[i];
 	}
-	bool flag = true;
-	int dx[] = {1, 0, -1, 0};
-	int dy[] = {0, 1, 0, -1};
-	for(int i = 0; i < n && flag; i++) {
-		for(int j = 0; j < n && flag; j++) {
-			int cnt = 0;
-			for(int d = 0; d < 4; d++) {
-				int a = i + dx[d], b = j + dy[d];
-				if(0 <= a && a < n && 0 <= b && b < n) {
-					if(m[a][b] == 'o') {
-						cnt++;
-					}
-				}
-			}
-			if(cnt % 2 != 0) {
-				flag = false;
-			}
+	sort(a, a + n);
+	for(int i = 0; i < n; i++) {
+		sum += (i + 1) * a[i];
+		if(i == n - 1) {
+			sum -= a[i];
 		}
 	}
-	cout << (flag?"YES":"NO") << endl;
+	cout << sum << endl;
 }

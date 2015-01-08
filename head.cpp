87bd4ccacad217ab26631be23d565be76ac562a2
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-01-08 13:01:57 -0500

    head.cpp & gitignore

--- head.cpp
+++ head.cpp
@@ -1,43 +1,73 @@
+/* base IO */
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <cstdlib>
-#include <cmath>
 #include <cctype>
+#include <fstream>
+#include <sstream>
 
-#include <string>
-#include <map>
-#include <set>
-#include <bitset>
-#include <queue>
-#include <deque>
-#include <list>
+/* data structure */
 #include <vector>
+#include <string>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <bitset>
 
+/* alrotihm and math */
 #include <algorithm>
-#include <complex>
+#include <random>
+#include <cmath>
+
+/* run-time */
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+
+/* system dependent */
+#include <sys/time.h>
+
 using namespace std;
 
-#define len(x) (x).size()
-#define VI vector<int> 
-
+#define VI vector<int>
+#define VD vector<double>
+#define PII pair<int, int>
+#define PDD pair<double, double>
 #define PB push_back
 #define MP make_pair
+#define len(x) ((x).size())
 
-typedef long long int64;
-typedef pair<int, int> pii;
-typedef pair<double, double> pdd;
-
-template <class T>
-inline T sqr(T x) { return x * x; }
-
-const int inf = ~0u >> 1;
+const int inf = INT_MAX;
+const long long inf_ll = 0x7f7f7f7f;
 const double eps = 1e-8;
-const double pi = acos(-1.0);
 
-int sgn(double x) {
-	return fabs(x) < eps ? 0:x < -eps?-1:1;
-}
+/*
+ * 0 1 2
+ * 3   4
+ * 5 6 7
+ */
+const int bfs_dy[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int bfs_dx[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+/*
+ *   3  
+ * 2   0
+ *   1  
+ */
+const int dfs_dy[] = {0, 1, 0, -1};
+const int dfs_dx[] = {1, 0, -1, 0};
+
+PII dir[] = {make_pair(0,1),make_pair(1,0),make_pair(0,-1),make_pair(-1,0)};
+/* TC HEADER END */
+
+class A {
+	public:
+		;
+};
 
 int main() {
+	A a;
 }

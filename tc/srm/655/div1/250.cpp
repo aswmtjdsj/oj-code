718f497d3c9afaafb17317e4103bf3bfc9442473
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-04-09 16:44:47 -0400

    tc srm 655 div1 250 searching from the last painting

--- tc/srm/655/div1/250.cpp
+++ tc/srm/655/div1/250.cpp
@@ -63,128 +63,78 @@ const int dfs_dx[] = {1, 0, -1, 0};
 PII dir[] = {make_pair(0,1),make_pair(1,0),make_pair(0,-1),make_pair(-1,0)};
 /* TC HEADER END */
 
-int n,m,k;
-const int M = 20 * 20 + 5;
-int a[M][M], b[M];
-bool l[M];
-
-long long gauss() {
-    int res = 0, r = 0;
-    memset(l,0,sizeof(bool) * n);
-    for(int i = 0; i < n; ++i){
-        for(int j = r; j < n; ++j){
-            if(a[j][i] == 1){
-                for(int k = i; k <= n; ++k)
-                    swap(a[j][k],a[r][k]);
-                break;
-            }
-        }
-        if(a[r][i] == 0){
-            if(a[r][n] != 0) return -1;//no solution
-            else{// free varbiable
-                ++res;continue;
-            }
-        }
-        for(int j = 0; j < n; ++j){
-            if(j != r && a[j][i] == 1){;
-                for(int k = i; k <= n; ++k)
-                    a[j][k] ^= a[r][k];
-            }
-        }
-        l[i] = true,++r;
-    }
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cout << a[i][j] << ' ';
-        } cout << "| " << a[i][m] << endl;
-    }
-    return res;
-    /*int i,j,row=1,col;
-    for (col=1;col<=m;++col) {   
-        for (i=row;i<=n;++i) {
-            if (a[i][col])
-                break ;
-        }
-        if (i>n)
-            continue ;
-        if (i!=row) {    
-            for (j=col;j<=m;++j) {
-                swap(a[row][j],a[i][j]);
-            }
-            swap(b[i],b[row]);
-        }
-        for (i=row+1;i<=n;++i) {
-            if (a[i][col]) {    
-                for (j=col;j<=m;++j) {
-                    a[i][j]^=a[row][j];
-                }
-
-                b[i]^=b[row];
-            }
-        }
-        ++row;
-    }
-    for (i=row;i<=n;++i)
-        if (b[i])
-            return -1;
-    return 1ll<<(long long)(m-row+1);*/
-}
-
 class BichromePainting {
     public:
         string isThatPossible(vector <string> board, int k) {
-            /*for(auto i: board) {
-              cout << i << endl;
-              }*/
-            int _n = board.size();
-            n = _n * _n;
-            m = _n * _n;
-            // cout << n << ' ' << m << endl;
-            for(int i = 0; i < _n; i++) {
-                for(int j = 0; j < _n; j++) {
-                    if(board[i][j] == 'B') {
-                        a[i * _n + j][m] = 1;
-                    } else {
-                        a[i * _n + j][m] = 0;
-                    }
-                    for(int p = 0; p < _n; p++) {
-                        for(int q = 0; q < _n; q++) {
-                            if(abs(p - i) < k && abs(q - j) < k) {
-                                a[i * _n + j][p * _n + q] = 1;
-                            } else {
-                                a[i * _n + j][p * _n + q] = 0;
+            int n = board.size();
+            auto B = board;
+            while(true) {
+                bool biu = false;
+                for(int i = 0; i < n - k + 1; i++) {
+                    for(int j = 0; j < n - k + 1; j++) {
+                        bool white = false, black = false;
+                        for(int p = 0; p < k; p++) {
+                            for(int q = 0; q < k; q++) {
+                                if(B[i+p][j+q] == 'B') {
+                                    black = true;
+                                }
+                                if(B[i+p][j+q] == 'W') {
+                                    white = true;
+                                }
+                            }
+                        }
+                        if((black && !white) || (!black && white)) {
+                            biu = true;
+                            for(int p = 0; p < k; p++) {
+                                for(int q = 0; q < k; q++) {
+                                    B[i+p][j+q] = '!';
+                                }
                             }
                         }
                     }
                 }
+                if(!biu) break;
             }
-            /*for(int i = 1; i <= n; i++) {
-              for(int j = 1; j <= m; j++) {
-              cout << a[i][j] << ' ';
-              } cout << "| " << b[i] << endl;
-              }*/
-            auto ans = gauss();
-            cout << ans << endl;
-            if(ans != -1) {
-                return "Possible";
-            } else {
-                return "Impossible";
+            bool flag = true;
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(B[i][j] != '!') {
+                        flag = false;
+                    }
+                }
             }
+            return flag?"Possible":"Impossible";
         }
 };
 
 int main() {
     BichromePainting a;
-    /*cout << a.isThatPossible({"BWBWBB",
-            "WBWBWB",
-            "BWBWBB",
-            "WBWBWB",
-            "BWBWBB",
-            "BBBBBB"}, 2) << endl;*/
     cout << a.isThatPossible({"BWBWBB",
+            "WBWBWB",
+            "BWBWBB",
+            "WBWBWB",
+            "BWBWBB",
+            "BBBBBB"}, 2) << endl;
+    cout << a.isThatPossible({
+            "BBBW",
+            "BWWW",
+            "BWWW",
+            "WWWW"}, 3) << endl;
+    cout << a.isThatPossible({
+            "BWBWBB",
             "WBWBBB",
             "BWBWBB",
             "WBWBBB",
             "BBBBBB",
             "BBBBBB"}, 2) << endl;
+    cout << a.isThatPossible({
+            "BWWWWBWBB",
+            "BWWBBWWWB",
+            "WWBBBBBWB",
+            "WBWWWWWBB",
+            "BWWBBBBWB",
+            "WBBWWBWBB",
+            "WWBBWWWBB",
+            "WWBWBBWBB",
+            "BWBBWWBBW"}, 7) << endl;
 }

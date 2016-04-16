685f7f86ba0097772eb32980a0a888e3b10ff291
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2016-04-15 23:41:32 -0400

    gcj 2016 r1a a small/large

---
+++ gcj/gcj16/1a/a.cpp
@@ -0,0 +1,89 @@
+/* base IO */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+#include <fstream>
+#include <sstream>
+
+/* data structure */
+#include <vector>
+#include <string>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <bitset>
+
+/* alrotihm and math */
+#include <algorithm>
+#include <random>
+#include <cmath>
+
+/* run-time */
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+
+/* system dependent */
+#include <sys/time.h>
+
+using namespace std;
+
+#define VI vector<int>
+#define VD vector<double>
+#define PII pair<int, int>
+#define PDD pair<double, double>
+#define PB push_back
+#define MP make_pair
+#define len(x) ((x).size())
+
+const int inf = INT_MAX;
+const long long inf_ll = 0x7f7f7f7f;
+const double eps = 1e-8;
+
+/*
+ * 0 1 2
+ * 3   4
+ * 5 6 7
+ */
+const int bfs_dy[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int bfs_dx[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+/*
+ *   3  
+ * 2   0
+ *   1  
+ */
+const int dfs_dy[] = {0, 1, 0, -1};
+const int dfs_dx[] = {1, 0, -1, 0};
+
+PII dir[] = {make_pair(0,1),make_pair(1,0),make_pair(0,-1),make_pair(-1,0)};
+/* TC HEADER END */
+
+int main() {
+    int T;
+    cin >> T;
+    for(int _ = 1; _ <= T; _++) {
+        string in;
+        cin >> in;
+        int l = in.size();
+        vector <string> c;
+        for (int i = 0; i < (1 << l); i++) {
+            string temp = "";
+            for (int j = 0; j < l; j++) {
+                if ((i >> j) & 1) {
+                    temp = temp + in[j];
+                } else {
+                    temp = in[j] + temp;
+                }
+            }
+            c.push_back(temp);
+        }
+        sort(c.begin(), c.end());
+        string ans = *c.rbegin();
+        printf("Case #%d: %s\n", _, ans.c_str());
+    }
+}
